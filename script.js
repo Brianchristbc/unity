a56ed53936bad00eb128f197fe2039eec19f53e1
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2025-04-07 20:59:21 +0800

    Fix phone number form field

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.getElementById("hero-form").addEventListener("submit", function (e) {
   e.preventDefault();
-
   let honeypot = document.getElementById("hp").value;
   if (honeypot !== "") {
-    console.warn("Bot detected. Submission ignored.");
     return;
   }
-
   let parms = {
     name: e.target.user_name.value,
-    email: e.target.user_email.value,
+    number: e.target.user_number.value, // Updated field
   };
-
-  emailjs
-    .send("service_gfxq4vh", "template_kq3frpq", parms)
-    .then(console.log("You should receive an email shortly!"))
-    .then(console.log("I'm not strong enough to do it in persons."));
+  emailjs.send("service_gfxq4vh", "template_kq3frpq", parms).then(function () {
+    document.querySelector(".submit-button").style.backgroundImage =
+      'url("./img/unity-form-submitted-button.png")';
+    document.querySelector(".phone-form").style.display = "none";
+    document.getElementById("form-confirmation").style.display = "block";
+  });
 });
